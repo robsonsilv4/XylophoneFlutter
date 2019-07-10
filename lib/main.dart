522b3425a69111760ee7af3ecbeaa260a18114e0
robsonsilv4 <robsonsilv410@gmail.com>
2019-07-10 08:41:53 -0300

    Aplicaçao finalizada

--- lib/main.dart
+++ lib/main.dart
@@ -12,62 +12,34 @@ class XylophoneApp extends StatelessWidget {
     player.play('note$number.wav');
   }
 
+  Expanded buildKey({int sound, Color color}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(sound);
+        },
+        child: null,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              FlatButton(
-                color: Colors.red,
-                onPressed: () {
-                  playSound(1);
-                },
-                child: null,
-              ),
-              FlatButton(
-                color: Colors.orange,
-                onPressed: () {
-                  playSound(2);
-                },
-                child: null,
-              ),
-              FlatButton(
-                color: Colors.yellow,
-                onPressed: () {
-                  playSound(3);
-                },
-                child: null,
-              ),
-              FlatButton(
-                color: Colors.green,
-                onPressed: () {
-                  playSound(4);
-                },
-                child: null,
-              ),
-              FlatButton(
-                color: Colors.teal,
-                onPressed: () {
-                  playSound(5);
-                },
-                child: null,
-              ),
-              FlatButton(
-                color: Colors.blue,
-                onPressed: () {
-                  playSound(6);
-                },
-                child: null,
-              ),
-              FlatButton(
-                color: Colors.purple,
-                onPressed: () {
-                  playSound(7);
-                },
-                child: null,
-              ),
+              buildKey(sound: 1, color: Colors.red),
+              buildKey(sound: 2, color: Colors.orange),
+              buildKey(sound: 3, color: Colors.yellow),
+              buildKey(sound: 4, color: Colors.green),
+              buildKey(sound: 5, color: Colors.teal),
+              buildKey(sound: 6, color: Colors.blue),
+              buildKey(sound: 7, color: Colors.purple),
             ],
           ),
         ),
