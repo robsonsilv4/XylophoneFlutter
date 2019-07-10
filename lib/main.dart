afe2005d0c97c83e96641e87590dd899530da651
robsonsilv4 <robsonsilv410@gmail.com>
2019-07-09 21:46:14 -0300

    Dependencia e audios

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() {
   runApp(XylophoneApp());
@@ -10,7 +11,15 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: FlatButton(
+              child: Text('Clique!'),
+              onPressed: () {
+                final player = AudioCache();
+                player.play('note1.wav');
+              },
+            ),
+          ),
         ),
       ),
     );
